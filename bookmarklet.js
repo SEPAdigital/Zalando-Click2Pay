acad4fc20c79e7724459e0e3101cc74b5aafce95
Rene Kapusta <rene@evo42.net>
2022-11-01 21:26:21 +0100

    Update bookmarklet.js
    fix after UI updates

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,16 @@
-let bankDetailsDirty = $('.z-sos-unfoldable-balance-notification__details-title').next().html().split('<br>'),
+let bankDetailsDirty = $('#main-content h2').nextElementSibling.getInnerHTML().split('<br>') || null,
     bankDetails = [],
-    amount = $('.z-sos-unfoldable-balance-notification__invoice-message').text().match(/[0-9]+([,.][0-9]+)?/)[0]
+    elAmount = $('p[data-testid="customer_order-detail-view_open-amount-text"]') || null,
+    amount = elAmount && elAmount.textContent.match(/[0-9]+([,.][0-9]+)?/)[0] || '0',
     newAmount = parseFloat(amount.replace(',', '.')),
-    $SEPAdigitalCode = $('#sepa-digital-pay-code');
+    $SEPAdigitalCode = $('#sepa-digital-pay-code') || null;
 
-bankDetailsDirty.forEach(function(line) {
-    let detail = line.split(':')[1].trim();
-    bankDetails.push(detail)
-})
+if (bankDetailsDirty) {
+    bankDetailsDirty.forEach(function(line) {
+        let detail = line.split(':')[1].trim();
+        bankDetails.push(detail)
+    });
+}
 
 let qrPay = {
     'recipient': bankDetails[0],
@@ -15,21 +18,46 @@ let qrPay = {
     'bic': bankDetails[2],
     'reference': bankDetails[6],
     'amount': amount},
-    priceHtml = $('div.z-sos-unfoldable-balance-notification__header-message').html().replace(amount, '<span class="sp-amount">' + amount + '</span>'),
-    qrUrl = 'https://dev.matthiasschaffer.com/bezahlcode/api.php?iban=' +  qrPay.iban + '&bic=' +  qrPay.bic + '&name=' +  qrPay.recipient + '&usage=' +  qrPay.reference + '&amount=',
-    qrImg = $('<img id="sepa-digital-pay-code" style="width: 220px; float: right;">').attr('src', qrUrl + qrPay.amount);
+    priceHtml = elAmount && elAmount.getInnerHTML().replace(amount, '<span class="sp-amount">' + amount + '</span>') || '0',
+    qrUrl = 'https://dev.matthiasschaffer.com/bezahlcode/api.php?iban=' +  qrPay.iban + '&bic=' +  qrPay.bic + '&name=' +  qrPay.recipient + '&usage=' +  qrPay.reference + '&amount=';
 
-if (!$SEPAdigitalCode.attr('src')) {
-    $('.z-sos-unfoldable-balance-notification__details-title').append(qrImg);
-    $SEPAdigitalCode = $('#sepa-digital-pay-code');
+    // console.log('*** priceHtml', priceHtml)
+    // console.log('*** qrUrl', qrUrl)
+
+let img = document.createElement('img');
+
+img.setAttribute('src', qrUrl + qrPay.amount);
+img.setAttribute('id', 'sepa-digital-pay-code');
+img.setAttribute('class', 'SEPAinstant-qr-code');
+img.setAttribute('style', 'width: 220px; float: left; margin: 0 0 25px 0;');
+
+let qrImg = img,
+    elQR = $('main div div').nextElementSibling.nextElementSibling;
+
+if (newAmount > 0) {
+    if (elQR && elQR.firstChild && elQR.firstChild.firstChild && elQR.firstChild.firstChild.classList.contains('isiDul')) {
+        $('main div div').nextElementSibling.nextElementSibling.prepend(qrImg);
+    } else if (!$SEPAdigitalCode) {
+        $('main div div').nextElementSibling.nextElementSibling.append(qrImg);
+    } else if ($SEPAdigitalCode && $SEPAdigitalCode.classList.contains('SEPAinstant-qr-code')) {
+        $SEPAdigitalCode.innerHTML = qrImg.outerHTML;
+    }
 }
 
-$('div.z-sos-unfoldable-balance-notification__header-message').html(priceHtml);
-$('div.z-sos-position-details__description-section').click(function() {
+$SEPAdigitalCode = $('#sepa-digital-pay-code');
+
+
+if (elAmount && elAmount.innerHTML) {
+    elAmount.innerHTML = priceHtml;
+}
+
+/*
+
+elAmount.click(function() {
         let $sel = $(this),
             $markedItems = $sel.find('.sp-return-this-marked'),
             cancelTxt = $sel.find('div.z-sos-position-details__cancellation-status span.z-sos-position-details__status-flag-text').first().text(),
-            lineAmount = parseFloat($sel.find('span.z-sos-position-details__price').text().match(/[0-9]+([,.][0-9]+)?/)[0].toString().replace(',', '.')),
+            lineAmount = parseFloat($sel.elAmount.textContent.match(/[0-9]+([,.][0-9]+)?/)[0].toString().replace(',', '.')),
             updatedAmount = 0;
 
         if (cancelTxt == 'Storniert') { return false; }
@@ -53,4 +81,6 @@ $('div.z-sos-position-details__description-section').click(function() {
 
         $('.sp-amount').text(newAmountFormatted).fadeOut().fadeIn();
         $SEPAdigitalCode.attr('src', qrUrl + newAmountFormatted).trigger('create');
-});
+})
+// */
+;
